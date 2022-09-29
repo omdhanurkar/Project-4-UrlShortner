a2fe9b4808ebbb37f1459c9f98d23fa0a155f701
VikasYadav14 <110589727+VikasYadav14@users.noreply.github.com>
2022-09-29 14:48:19 +0530

    done

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -2,19 +2,22 @@ const urlModel = require("../Models/urlModel")
 const shortId = require("shortid")
 const validUrl = require("valid-url")
 
+function isPresent(value) {
+    if(typeof(value)==="undefined" || typeof(value)===null) return false
+    if(typeof(value)==="string" && value.trim().length == 0) return false
+    return true
+ }
+
 
 const shortUrl = async function (req, res) {
     try {
         let { longUrl } = req.body;
 
-        if (Object.keys(req.body).length == 0)
-            return res.status(400).send({ status: false, message: "Plz enter some data in body" })
+        if (Object.keys(req.body).length == 0) return res.status(400).send({ status: false, message: "Plz enter some data in body" })
 
-        if (!longUrl)
-            return res.status(400).send({ status: false, message: "longUrl is mandatory" })
+        if (!isPresent(longUrl)) return res.status(400).send({ status: false, message: "longUrl is mandatory" })
 
-        if (!validUrl.isUri(longUrl))
-            return res.status(400).send({ status: false, message: "longUrl is not Valid" })
+        if (!validUrl.isUri(longUrl)) return res.status(400).send({ status: false, message: "longUrl is not Valid" })
 
         const checkUrl = await urlModel.findOne({ longUrl }).select({ _id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 })
 
@@ -39,11 +42,11 @@ const getUrl = async function (req, res) {
     try {
         let urlCode = req.params.urlCode
 
-        if (!urlCode)
-            return res.status(400).send({ status: false, message: "longUrl is mandatory" })
-
+        if (!urlCode) return res.status(400).send({ status: false, message: "longUrl is mandatory" })
 
         const details = await urlModel.findOne({ urlCode }).select({ _id: 0, longUrl: 1 })
+
+        if(!details) return res.status(404).send({ status: false, message: "No data found" })
         return res.status(200).send({ status: true, data: details })
 
     } catch (error) {

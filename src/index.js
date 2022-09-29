1af8efbe0f11e7ca36c5c89e2840b285ab5b658f
Omprakash <omdhanurkar444@gmail.com>
2022-09-29 11:54:14 +0530

    1st day

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const route = require('./routes/route.js');
+const  mongoose  = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+mongoose.connect("mongodb+srv://group60Database:pdMj2eV7oExXwWKc@group60database.jr8rh6i.mongodb.net/group60Database", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+//----------wrong api edge case--------------------------------------------
+// app.use((req, res, next) => {
+//     res.status(400).send({ status: false, error: "URL is wrong" });
+// })
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
